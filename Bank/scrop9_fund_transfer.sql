7d60bc2a04b6bbeec1f56cb9777855de2c9a2fc8
Mujju-palaan <mdmujahid97@gmail.com>
2025-08-12 17:29:53 +0530

    scrop9

---
+++ Bank/scrop9_fund_transfer.sql
@@ -0,0 +1,17 @@
+-- 9)Stored Procedure for Fund Transfer
+
+-- Create a stored procedure to manage fund transfer details. 
+-- The procedure should perform the following operations: insert a record into the fund transfer table, 
+-- log the transaction in the transaction table, update the account balance, and record the changes 
+-- in the account history.
+
+DELIMITER $$
+create procedure scrop9_fund_transfer(
+	IN 
+    ,IN
+)
+
+BEGIN
+
+END $$
+DELIMITER ;
