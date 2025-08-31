b20b4853e390aa204e1e1bce482b8269a3594a61
Mujju-palaan <mdmujahid97@gmail.com>
2025-08-31 16:18:52 +0530

    DML completed

--- Store_management_system/Mujju-MySQL-Store-C06(a)-dml.sql
+++ Store_management_system/Mujju-MySQL-Store-C06(a)-dml.sql
@@ -1,8 +1,8 @@
---Store DML(Data Manipulation Language) ASSIGNMENT
---⁠ ⁠Mujju-postgres-Store-Chapter06-dml
+-- Store DML(Data Manipulation Language) ASSIGNMENT
+-- Mujju-postgres-Store-Chapter06-dml
 
---Question 1: Insert Data into all Tables
---Write SQL queries to populate each table created in the Chapter 5 assignment with at least 10 rows of data.
+-- Question 1: Insert Data into all Tables
+-- Write SQL queries to populate each table created in the Chapter 5 assignment with at least 10 rows of data.
 insert into city(country_name,city_name)
 values ('USA','New York'),
 		('India','Hyderabad'),
@@ -14,8 +14,9 @@ values ('USA','New York'),
 		('AUS','Sydney'),
 		('Poland','Warsaw'),
 		('Pakistan','lahore');
---select * from city
+-- select * from city
 
+-- select * from store
 insert into store(store_name,location,phone_number,city_id)
 values ('Croma','kukatpally','12345678901',1),
 		('Concu','Sainikpuri','12345678902',2),
@@ -29,8 +30,29 @@ values ('Croma','kukatpally','12345678901',1),
 		('Wall Mart','Kompally','12345678909',10)
 ;
 
---select * from product;
-INSERT INTO product(product_name, category,price,description,sku)
+-- alter table store add column quantity_in_stock int;
+
+-- UPDATE store
+-- SET quantity_in_stock = CASE store_id
+-- WHEN 1 THEN 111
+-- WHEN 2 THEN 222
+-- WHEN 3 THEN 333
+-- WHEN 4 THEN 444
+-- WHEN 5 THEN 555
+-- WHEN 6 THEN 666
+-- WHEN 7 THEN 777
+-- WHEN 8 THEN 888
+-- WHEN 9 THEN 999
+-- WHEN 10 THEN 1011
+-- WHEN 11 THEN 1111
+-- ELSE quantity_in_stock
+-- END
+-- WHERE store_id IN(1,2,3,4,5,6,7,8,9,10,11)
+-- ;
+-- alter table store drop column quantity_in_stock;
+
+-- select * from product;
+INSERT INTO product(product_name, category, price , description, sku)
 values
 	('Coca-Cola', 'drinks', 1.49, 'A classic cola soft drink.', 'DRINK-COCA-001'),
     ('Pepsi', 'drinks', 1.49, 'Refreshing cola beverage.', 'DRINK-PEPSI-002'),
@@ -49,7 +71,7 @@ values
     ('Pineapple', 'fruits', 3.99, 'Tropical and tangy pineapple.', 'FRUIT-PINEAPPLE-005')
 ;
 
---select * from supplier;
+-- select * from supplier;
 insert into supplier(supplier_name, contact_number)
 values ('Global Supplies Co.', '123-456-7890'),
     ('Fresh Produce Inc.', '234-567-8901'),
@@ -63,7 +85,7 @@ values ('Global Supplies Co.', '123-456-7890'),
     ('BrightPath Logistics', '012-345-6789')
 ;
 
---select * from SUPPLIER_PRODUCT;
+-- select * from SUPPLIER_PRODUCT;
 insert into SUPPLIER_PRODUCT(supplier_id, product_id)
 values(1,1),
 		(2,2),
@@ -77,8 +99,8 @@ values(1,1),
 		(10,10)
 ;
 
---select * from inventory;
-insert into inventory(store_id,product_id,quantity_in_stock,supplier_id)
+-- select * from inventory;
+insert into inventory(store_id, product_id, quantity_in_stock, supplier_id)
 values(1,1,123444,1),
 		(2,2,123,2),
 		(3,3,3444,3),
@@ -91,7 +113,7 @@ values(1,1,123444,1),
 		(10,10,123444,10)
 ;
 
---select * from customer;
+-- select * from customer;
 insert into customer(first_name,last_name,gender,id_type,id_number,email,phone_number,address,city_id)
 values('Alice', 'Walker', 'F', 'passport', 'P123456789', 'alice.walker@example.com', '123-456-7890', '45 Maple Lane', 1),
     ('Bob', 'Johnson', 'M', 'driving license', 'DL987654321', 'bob.johnson@example.com', '234-567-8901', '12 Pine Avenue', 2),
@@ -105,42 +127,24 @@ values('Alice', 'Walker', 'F', 'passport', 'P123456789', 'alice.walker@example.c
     ('Jack', 'Lee', 'M', 'driving license', 'DL778899001', 'jack.lee@example.com', '012-345-6789', '45 Redwood Avenue', 5)
 ;
 
---select * from orders;
-alter table store add column quantity_in_stock int;
-
-UPDATE store
-SET quantity_in_stock = CASE store_id
-WHEN 1 THEN 111
-WHEN 2 THEN 222
-WHEN 3 THEN 333
-WHEN 4 THEN 444
-WHEN 5 THEN 555
-WHEN 6 THEN 666
-WHEN 7 THEN 777
-WHEN 8 THEN 888
-WHEN 9 THEN 999
-WHEN 10 THEN 1011
-WHEN 11 THEN 1111
-ELSE quantity_in_stock
-END
-WHERE store_id IN(1,2,3,4,5,6,7,8,9,10,11);
-
---check (order_status in ('Accepted','Packing','Ready for Delivery','Shipping','Delivered','Paid','Closed'))
+-- select * from orders;
+-- check (order_status in ('Accepted','Packing','Ready for Delivery','Shipping','Delivered','Paid','Closed'))
 insert into orders(order_date, total_amount, delivered_duration, customer_id, store_id, order_status)
-values ('2025-01-15', 150.75, '02:00:00', 1, 1, 'Accepted'),
-    ('2025-02-20', 85.50, '03:30:00', 2, 2, 'Packing'),
-    ('2025-03-10', 230.00, '01:15:00', 3, 3, 'Delivered'),
-    ('2025-04-05', 120.99, '12:15:00', 4, 4, 'Ready for Delivery'),
-    ('2025-05-25', 95.00, '04:45:00', 5, 5, 'Delivered'),
-    ('2025-06-12', 310.40, '02:20:00', 6, 1, 'Shipping'),
-    ('2025-07-07', 50.00, '14:45:00', 7, 2, 'Paid'),
-    ('2025-08-18', 200.00, '05:50:00', 8, 3, 'Closed'),
-    ('2025-09-22', 175.00, '03:10:00', 9, 4, 'Delivered'),
-    ('2025-10-30', 65.99, '13:13:00', 10, 5, 'Accepted')
+values ('2025-01-15', 150.75, '2025-01-15 02:00:00', 1, 1, 'Accepted'),
+    ('2025-02-20', 85.50, '2025-02-20 03:30:00', 2, 2, 'Packing'),
+    ('2025-03-10', 230.00, '2025-03-10 01:15:00', 3, 3, 'Delivered'),
+    ('2025-04-05', 120.99, '2025-04-05 12:15:00', 4, 4, 'Ready for Delivery'),
+    ('2025-05-25', 95.00, '2025-05-25 04:45:00', 5, 5, 'Delivered'),
+    ('2025-06-12', 310.40, '2025-06-12 02:20:00', 6, 1, 'Shipping'),
+    ('2025-07-07', 50.00, '2025-07-07 14:45:00', 7, 2, 'Paid'),
+    ('2025-08-18', 200.00, '2025-08-18 05:50:00', 8, 3, 'Closed'),
+    ('2025-09-22', 175.00, '2025-09-22 03:10:00', 9, 4, 'Delivered'),
+    ('2025-10-30', 65.99, '2025-10-30 13:13:00', 10, 5, 'Accepted')
 ;
 
---select * from order_product;
-insert into order_product(order_id,product_id,units,unit_rate,total_amount)
+
+-- select * from order_product;
+insert into order_product(order_id, product_id, units, unit_rate, total_amount)
 values (1, 1, 3, 50.00, 150.00),
     (1, 2, 2, 75.00, 150.00),
     (2, 3, 5, 20.00, 100.00),
@@ -153,7 +157,7 @@ values (1, 1, 3, 50.00, 150.00),
     (8, 10, 8, 25.00, 200.00)
 ;
 
---select * from payment
+-- select * from payment
 insert into payment(payment_date, amount_paid, order_id)
 values ('2025-01-16', 150.00, 1),
     ('2025-02-21', 100.00, 2),
@@ -167,7 +171,7 @@ values ('2025-01-16', 150.00, 1),
     ('2025-10-31', 65.99, 10)
 ;
 
---select * from employee;
+-- select * from employee;
 insert into employee(first_name, last_name, email, hire_date, salary, store_id, phone_number)
 values ('John', 'Doe', 'john.doe@example.com', '2025-01-10', 55000.00, 1, '123-456-7890'),
     ('Jane', 'Smith', 'jane.smith@example.com', '2024-11-15', 48000.00, 2, '234-567-8901'),
@@ -181,7 +185,7 @@ values ('John', 'Doe', 'john.doe@example.com', '2025-01-10', 55000.00, 1, '123-4
     ('William', 'Martinez', 'william.martinez@example.com', '2025-02-28', 58000.00, 5, '012-345-6789')
 ;
 
---select * from employee_login;
+-- select * from employee_login;
 insert into employee_login(employee_id, login_id, password, active_flag)
 values  (1, 'john.doe123', 'encrypted_password_1', 1),
     (2, 'jane_smith@company.com', 'encrypted_password_2', 1),
@@ -195,7 +199,7 @@ values  (1, 'john.doe123', 'encrypted_password_1', 1),
     (10, 'william_martinez22', 'encrypted_password_10', 1)
 ;
 
---select * from discount
+-- select * from discount
 insert into discount(discount_code, discount_percentage)
 values ('DISC10', 10),
     ('SUMMER15', 15),
@@ -209,7 +213,7 @@ values ('DISC10', 10),
     ('FLASHSALE7', 7.50)
 ;
 
---select * from audit_log;
+-- select * from audit_log;
 insert into audit_log(log_date, action, employee_id)
 values  ('2025-01-15', 'Logged in', 1),
     ('2025-01-16', 'Updated product details', 2),
@@ -222,9 +226,9 @@ values  ('2025-01-15', 'Logged in', 1),
     ('2025-01-23', 'Deleted product from inventory', 9),
     ('2025-01-24', 'Updated employee details', 10);
 
---select * from return_policy;
+-- select * from return_policy;
 
---select * from order_discount;
+-- select * from order_discount;
 insert into order_discount(order_id, discount_id)
 values (1, 1),
     (2, 2),
